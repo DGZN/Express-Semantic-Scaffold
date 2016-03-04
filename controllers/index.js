2f65db9663f759c1e47eca866e5971a8512a6c51
Keiichi Lindley <digizn@gmail.com>
2016-03-04 16:29:37 -0700

    Add details page

--- controllers/index.js
+++ controllers/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+router.get('/details', function(req, res, next) {
+  res.render('details');
+});
+
 module.exports = router;
