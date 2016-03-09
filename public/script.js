4076dad3bdeb88f2ffea48349d40690bd47b6b85
Keiichi Lindley <digizn@gmail.com>
2016-03-08 17:09:21 -0700

    Add watchlist sidebar to series page

--- public/script.js
+++ public/script.js
@@ -14,53 +14,62 @@ $(document)
     // ;
 
     // create sidebar and attach to menu open
-    $('.ui.sidebar')
+    $('#watchlist-sidebar')
+      .sidebar('attach events', '.watchlist.item')
+    ;
+
+    $('#seasons-dropdown-filter')
+      .dropdown()
+    ;
+
+
+    $('#nav-sidebar')
       .sidebar('attach events', '.toc.item')
     ;
 
-    $('#ep12').progress({
+    $('.ep12').progress({
       percent: 75
     , showActivity: false
     , className: 'error'
     , text: { percent : '' }
     });
 
-    $('#ep13').progress({
+    $('.ep13').progress({
       percent: 30
     , showActivity: false
     , className: 'error'
     , text: { percent : '' }
     });
 
-    $('#ep14').progress({
+    $('.ep14').progress({
       percent: 100
     , showActivity: false
     , className: 'success'
     , text: { percent : '' }
     });
 
-    $('#ep15').progress({
+    $('.ep15').progress({
       percent: 0
     , showActivity: false
     , className: 'error'
     , text: { percent : '' }
     });
 
-    $('#ep16').progress({
+    $('.ep16').progress({
       percent: 0
     , showActivity: false
     , className: 'error'
     , text: { percent : '' }
     });
 
-    $('#ep17').progress({
+    $('.ep17').progress({
       percent: 0
     , showActivity: false
     , className: 'error'
     , text: { percent : '' }
     });
 
-    $('#ep18').progress({
+    $('.ep18').progress({
       percent: 0
     , showActivity: false
     , className: 'error'
