12ce36b3690595c201bc545fb33079fa2208cc2f
Keiichi Lindley <digizn@gmail.com>
2016-03-10 21:22:51 -0700

    Add movies page

--- controllers/index.js
+++ controllers/index.js
@@ -6,12 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-router.get('/details', function(req, res, next) {
-  res.render('details');
+router.get('/movies', function(req, res, next) {
+  res.render('movies');
 });
 
 router.get('/series', function(req, res, next) {
   res.render('series');
+});
+
+router.get('/details', function(req, res, next) {
+  res.render('details');
 });
 
 module.exports = router;
