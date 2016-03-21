34588886764079b9ff1d9d3407444e4b21fb5f6b
Keiichi Lindley <digizn@gmail.com>
2016-03-21 14:48:13 -0600

    Remove production year from video detail component

--- public/js/video-detail.js
+++ public/js/video-detail.js
@@ -23,14 +23,12 @@ const VideoDetail = React.createClass({
     var movie = {
       name: ''
     , description: ''
-    , productionYear: 'Produced ' + this.state.movie['production_year']
     , player: ''
     }
     if (this.state.movie) {
       var movie = {
         name: this.state.movie.meta[this.props.language].name
       , description: this.state.movie.meta[this.props.language].description
-      , productionYear: 'Produced ' + this.state.movie['production_year']
       , player: this.getPlayer(this.state.movie['video_url'])
       }
     }
@@ -46,7 +44,7 @@ const VideoDetail = React.createClass({
               <div className="ui one column grid center">
                 <div className="row">
                   <div className="column tight">
-                    <div className="movie-details"><span>R</span><span>123 min</span><span>{movie.productionYear}</span><span>5.1</span><span>HD</span></div>
+                    <div className="movie-details"><span>R</span><span>123 min</span><span></span><span>5.1</span><span>HD</span></div>
                     <div className="movie-actions"><img src="/images/play-button.png" className="ui image"/><img src="/images/add-button.png" className="ui image"/><img src="/images/social-share-circle-icons.png" className="ui image social-share"/></div>
                   </div>
                 </div>
