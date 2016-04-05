b127a92106f7440c40bf8b7b42e74bd3cfb5ead0
Keiichi Lindley <digizn@gmail.com>
2016-04-05 11:49:57 -0600

    Remove login popup on video player pages

--- modules/Play.js
+++ modules/Play.js
@@ -4,8 +4,8 @@ import VideoDetail from './VideoDetail'
 
 export default React.createClass({
   render() {
-    if (!this.props.user)
-      this.props.myAccount()
+    // if (!this.props.user)
+    //   this.props.myAccount()
     var source = '/plays/' + this.props.params.id
     return (
       <VideoDetail {...this.props} source={source} />
--- modules/Season.js
+++ modules/Season.js
@@ -4,8 +4,8 @@ import SeasonDetail from './SeasonDetail'
 
 export default React.createClass({
   render() {
-    if (!this.props.user)
-      this.props.myAccount()
+    // if (!this.props.user)
+    //   this.props.myAccount()
     var source = '/series/' + this.props.params.id
     return (
       <SeasonDetail {...this.props} source={source} />
